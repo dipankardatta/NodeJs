05141bbe58c93a10d7a9a1a6d234da637b21bd58
dipankardatta <dipankarzeus10@gmail.com>
2023-04-18 18:41:33 +0530

    using URL/message

--- first-app.js
+++ first-app.js
@@ -1,8 +1,29 @@
 const http = require('http');
  
 const server = http.createServer((req,res)=> {
-    console.log('Dipankar');
-
+    const url = req.url;
+    if (url === '/home'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title> My first Page </title></head>')
+    res.write('<body><h1> Welcome Home</h1></body>')
+    res.write('</html>')
+    res.end()
+    } else if( url === '/about'){
+        res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title> My first Page </title></head>')
+    res.write('<body><h1> Welcome to about us Page</h1></body>')
+    res.write('</html>')
+    res.end()
+    }else{
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title> My first Page </title></head>')
+    res.write('<body><h1> Welcome to my Node.js Project</h1></body>')
+    res.write('</html>')
+    res.end()
+    }
 });
 
 server.listen(4000);
