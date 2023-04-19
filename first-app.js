ea07358b7aa4f7ba866ef6bfc62b555ef1f2997c
dipankardatta <dipankarzeus10@gmail.com>
2023-04-19 21:12:58 +0530

    Using use and body parser

--- first-app.js
+++ first-app.js
@@ -1,16 +1,23 @@
-
-
 const express = require('express')
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the Middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">SIZE</button><button type="submit">ADD PRODUCT</button></form>');
 });
 
-app.use((req,res,next)=>{
-    console.log('In another MIddleware');
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body.title)
+    console.log(req.body.size)
+    res.redirect('/')
+})
+
+
+
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello from Express JS</h1>')
 })
 
