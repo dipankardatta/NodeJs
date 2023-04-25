5f9a30a82fcf36aabf0b6fbbee329fd250fb7f02
dipankardatta <dipankarzeus10@gmail.com>
2023-04-25 19:19:02 +0530

    Using Sequelize to create database

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize ('node-complete','root','123456',{
+    dialect:'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
-});
+})
 
-module.exports = pool.promise();
+module.exports = sequelize
