c36972eb41a4149b5b65236ceaeefd633fc80ec0
dipankardatta <dipankarzeus10@gmail.com>
2023-04-27 02:13:06 +0530

    Full STACK CALL APP

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const User = require('./models/user')
 
-const errorController = require('./controllers/error');
+//const errorController = require('./controllers/error');
 const sequelize = require('./util/database')
 
 const app = express();
+const cors = require('cors')
+app.use(cors())
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,15 +24,70 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.get404);
+// app.use(errorController.get404);
 
 sequelize.sync()
 .then(result =>{
     // console.log(result);
-    app.listen(3000);
+    // app.listen(7000);
 })
 .catch(err => {
     console.log(err)
 })
 
+app.post('/user/add-user', async (req, res, next) => {
+    try {
+      console.log(req.body);
+  
+      if (!req.body.phoneNumber) {
+        throw new Error('Phone Number Mandatory');
+      }
+  
+      const { name, email, phoneNumber } = req.body;
+  
+      const data = await User.create({
+        name,
+        email,
+        phoneNumber,
+      });
+  
+      res.status(201).json({ newUserDetail: data });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        error: err.message,
+      });
+    }
+  });
+  
+
+  app.get('/user/get-users', async (req, res, next) => {
+    try {
+      const users = await User.findAll();
+      res.status(200).json({ allUsers: users });
+    } catch (err) {
+      res.status(500).json({ error: err });
+    }
+  });
+  
+
+app.delete('/user/delete-user/:id', async (req,res,next)=>{
+    const userId = req.params.id;
+    User.findByPk(userId)
+    .then(user=>{
+      user.destroy()
+    })
+    .then(result=>{
+      console.log('Deleted')
+    })
+    .catch(e=>console.log(e))
+})
+
+sequelize.sync()
+.then(
+    app.listen(3000)
+)
+.catch(err=>{
+    console.log(err)
+})
 
---
+++ main.js
@@ -0,0 +1,82 @@
+function getval(event){
+    event.preventDefault();
+    alert("The form has been submitted");
+    var name = event.target.fname.value;
+    var mailid = event.target.mail.value;
+    var phone = event.target.phone.value;
+
+let ob = {
+    name,
+    mailid,
+    phone
+};
+
+axios.post('http://localhost:3000/user/add-user',ob)
+.then(val =>{
+    // console.log(val)
+    UIelement(val.data.newUserDetail)
+})
+.catch(err=>console.log(err));
+}
+const ul = document.querySelector('#users');
+//var itemlist = document.querySelector('.users');
+
+window.addEventListener("load",(e)=>{
+    e.preventDefault();
+axios.get('http://localhost:3000/user/get-user')
+.then(val=>{
+
+    val.data.allUsers.forEach(item => {
+         UIelement(item);
+    // });
+})
+})
+.catch(e=>console.log(e))
+   
+})
+
+function UIelement(ob){
+
+        var li=document.createElement('li');
+        li.appendChild(document.createTextNode(`${ob.name} : ${ob.mailid} : ${ob.phone}`) );
+        li.id=ob.phone;
+        //console.log(li);
+
+        
+        var newele=document.createElement('button');
+        newele.className=' btn btn-danger btn-sm m-1 float-right';
+        newele.appendChild(document.createTextNode('Delete'));
+        newele.id='delbtn';
+        newele.addEventListener("click", ()=>
+        {   
+            if (confirm('delete me?'))
+            {   //console.log(li)
+                //var item = event.target.parentElement;
+                //ul.removeChild(item)
+                //localStorage.removeItem(item.id)
+                // localStorage.removeItem(ob.mail)
+                ul.removeChild(li);
+
+                axios.delete(`http://localhost:3000/user/delete-user/${ob.id}`)
+                .then(val=>console.log(val.data))
+
+            }
+        }
+        );
+
+        // var editele = document.createElement('button');
+        // editele.className='btn btn-secondary btn-sm m-1 float-right'
+        // editele.appendChild(document.createTextNode('Edit'));
+        // editele.onclick=()=>{
+        //     // localStorage.removeItem(ob.mail);
+        //     axios.delete(`https://crudcrud.com/api/7f911fed0d704e9684b1c60fcc4099a2/appointment/${ob._id}`)
+        //     document.getElementById('fullname').value = ob.name1;
+        //     document.getElementById('mailid').value = ob.mail;
+        //     document.getElementById('phoneno').value = ob.phone;
+        //     ul.removeChild(li)
+        // }
+
+        li.appendChild(newele);
+        // li.appendChild(editele);
+        ul.appendChild(li);
+}
