ce749eb68abb5180e8c0e4aea33906c603da7d0c
dipankardatta <dipankarzeus10@gmail.com>
2023-04-28 11:32:00 +0530

    has to many relationship

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const User = require('./models/user')
+// const User = require('./models/formuser')
 
-//const errorController = require('./controllers/error');
+const errorController = require('./controllers/error');
 const sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
 
 const app = express();
 const cors = require('cors')
@@ -21,73 +23,109 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+  User.findByPk(1)
+  .then(user => {
+    req.user = user;
+    next()
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// app.use(errorController.get404);
+app.use(errorController.get404);
 
-sequelize.sync()
-.then(result =>{
-    // console.log(result);
-    // app.listen(7000);
-})
-.catch(err => {
+Product.belongsTo(User,{constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product)
+
+sequelize
+// .sync({force: true})
+  .sync()
+  .then(result => {
+    return User.findByPk(1)
+  })
+  .then(user => {
+    if (!user){
+      return User.create({name:'Dipankar', email: 'dipankar@gmail.com'})
+    }
+    return user;
+  })
+  .then( user => {
+    // console.log(user)
+    app.listen(7000);
+  })
+  .catch(err => {
     console.log(err)
-})
+  })
 
-app.post('/user/add-user', async (req, res, next) => {
-    try {
-      console.log(req.body);
-  
-      if (!req.body.phoneNumber) {
-        throw new Error('Phone Number Mandatory');
-      }
-  
-      const { name, email, phoneNumber } = req.body;
-  
-      const data = await User.create({
-        name,
-        email,
-        phoneNumber,
-      });
-  
-      res.status(201).json({ newUserDetail: data });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({
-        error: err.message,
-      });
+app.get('/user/add-users', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json({ allUsers: users });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
+
+app.post('/user/add-users', async (req, res, next) => {
+  try {
+    console.log(req.body);
+
+    if (!req.body.phonenumber) { // corrected here
+      throw new Error('Phone Number Mandatory');
     }
-  });
-  
 
-  app.get('/user/get-users', async (req, res, next) => {
-    try {
-      const users = await User.findAll();
-      res.status(200).json({ allUsers: users });
-    } catch (err) {
-      res.status(500).json({ error: err });
-    }
-  });
-  
+    const { name, email, phonenumber } = req.body;
 
-app.delete('/user/delete-user/:id', async (req,res,next)=>{
-    const userId = req.params.id;
-    User.findByPk(userId)
-    .then(user=>{
+    const data = await User.create({
+      name,
+      email,
+      phonenumber,
+    });
+
+    res.status(201).json({ newUserDetail: data });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
+
+
+
+app.get('/user/get-users', async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json({ allUsers: users });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
+
+app.delete('/user/delete-user/:id', async (req, res, next) => {
+  const userId = req.params.id;
+  User.findByPk(userId)
+    .then(user => {
       user.destroy()
     })
-    .then(result=>{
+    .then(result => {
       console.log('Deleted')
     })
-    .catch(e=>console.log(e))
+    .catch(e => console.log(e))
 })
 
-sequelize.sync()
-.then(
-    app.listen(3000)
-)
-.catch(err=>{
-    console.log(err)
-})
+// sequelize.sync()
+//   .then(
+//     app.listen(3000)
+//   )
+//   .catch(err => {
+//     console.log(err)
+//   })
 
--- models/user.js
+++ models/user.js
@@ -1,25 +1,14 @@
 const Sequelize = require('sequelize');
-
 const sequelize = require('../util/database')
-
 const User = sequelize.define('user',{
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement : true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name:{
-    type: Sequelize.STRING
-  },
-  email: {
-    type: Sequelize.STRING,
-    unique: true},
-  phoneNumber: {
-    type: Sequelize.DOUBLE,
-    allowNull: false,
-    unique: true
-  }
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name:Sequelize.STRING,
+    email: Sequelize.STRING
 })
 
-module.exports = User 
+module.exports = User
