1f1f54c984d558714c30631ece23ac70692b5809
dipankardatta <dipankarzeus10@gmail.com>
2023-04-28 13:24:24 +0530

    one to many relationship and adding cart

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database')
 const Product = require('./models/product')
 const User = require('./models/user')
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
 
 const app = express();
 const cors = require('cors')
@@ -41,6 +43,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User,{constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product)
+User.hasOne(Cart)
+Cart.belongsTo(User)
+Cart.belongsToMany(Product,{through: CartItem })
+Product.belongsToMany (Cart,{through: CartItem })
 
 sequelize
 // .sync({force: true})
@@ -56,11 +62,19 @@ sequelize
   })
   .then( user => {
     // console.log(user)
+    return user.createCart()
+    
+  }).then( cart => {
     app.listen(7000);
   })
   .catch(err => {
     console.log(err)
   })
+
+
+
+
+
 
 app.get('/user/add-users', async (req, res, next) => {
   try {
